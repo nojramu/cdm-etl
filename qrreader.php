e402206e31ebaf0c0ed4a503c795aa007619c0c7
nojram <nojramu5@gmail.com>
2024-12-27 10:57:51 +0800

    made it turned back

--- qrreader.php
+++ qrreader.php
@@ -4,26 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Code Reader</title>
-    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <title>Document</title>
 </head>
 
 <body>
-    <div id="reader"></div>
-    <p id="result"></p>
-
+    <video id="preview"></video>
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
     <script type="text/javascript">
-        function onScanSuccess(qrCodeMessage) {
-            document.getElementById("result").innerHTML = qrCodeMessage;
-        }
-
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", {
-                fps: 10,
-                qrbox: 250
-            });
-        html5QrcodeScanner.render(onScanSuccess);
+        var scanner = new Instascan.Scanner({
+            video: document.getElementById('preview'),
+            scanPeriod: 5
+        });
+        scanner.addListener('scan', function(content) {
+            alert(content);
+        });
+        Instascan.Camera.getCameras().then(function(cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function(e) {
+            console.error(e);
+        });
     </script>
+    <br>
+    <!-- Button to exit and return to the main page -->
+    <button onclick="location.href='index.php'">Exit</button>
 </body>
-
-</html>
